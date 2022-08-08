4ca310961c97e6ed27f19622d3dd01d6230fc0b6
mochi-666 <94042218+mochi-666@users.noreply.github.com>
2022-08-08 23:16:25 +0900

    update

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+  public static void main(String[] args) {
+	  final int LUCKY_NUMBER = 2;
+	  String str = "望月敬介";
+	  System.out.println(str);
+	  str = "常識は破ってもいいが非常識であってはならない";
+	  System.out.println(str);
+	  System.out.println(LUCKY_NUMBER);
+  }
+}
