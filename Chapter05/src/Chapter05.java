36b84243966941bc6356c904191030095ff87700
mochi-666 <94042218+mochi-666@users.noreply.github.com>
2022-08-12 01:18:44 +0900

    update Chapter05

---
+++ Chapter05/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+  public static void main(String[] args) {
+	  long l = 1000000L;
+	  System.out.println(l);
+	  double d = 3.14;
+	  System.out.println(d);
+	  boolean b = true;
+	  System.out.println(b);
+	  
+	  
+	  Integer i = 200;
+	  System.out.println(i);
+	  Float f = 2.2424242424f;
+	  System.out.println(f);
+	  Character c = 'う';
+	  System.out.println(c);
+	  
+	  
+	  int[] array = { 0, 10, 20, 30, 40 };
+	  System.out.println(array[3]);
+  }
+}
